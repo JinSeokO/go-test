59aca409ac7d78cedf4b9b1cbbfe42d0e6f80923
오진석 <freejin4802@gmail.com>
2021-11-26 03:17:48 +0900

    update todo service

--- app.go
+++ app.go
@@ -5,51 +5,8 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinseoko/real-life-go-api/common"
-	"github.com/jinseoko/real-life-go-api/repository"
 	"log"
-	"net/http"
 )
-
-type TestStruct struct {
-	num int
-}
-
-func NewTestStruct(num int) TestStruct{
-	return TestStruct{num: num}
-}
-
-func (t TestStruct) update(num int) TestStruct  {
-	t.num = num
-	return t
-}
-
-func withTrxMiddleWare(db *sql.DB, o *sql.TxOptions) func(c *gin.Context) {
-	return func(c *gin.Context) {
-		trx, err := db.BeginTx(c, o)
-		if err != nil {
-			log.Println("begin transaction error")
-		}
-
-		defer func() {
-			if r := recover(); r != nil {
-				trx.Rollback()
-				log.Println(r, "trx do rollback")
-			}
-		}()
-
-		c.Set("trx", trx)
-
-		c.Next()
-
-		if c.Writer.Status() == http.StatusOK || c.Writer.Status() == http.StatusCreated {
-			if err := trx.Commit(); err != nil {
-				log.Printf("trx commit error: %s", err)
-			}
-		} else {
-			_ = trx.Rollback()
-		}
-	}
-}
 
 func main() {
 	router := gin.Default()
@@ -57,17 +14,7 @@ func main() {
 
 	dbConfig := common.GetMysqlDbConfig("localhost:3307", "root", "adfaie83ma", "golang-test")
 	db, err := sql.Open("mysql", dbConfig.FormatDSN())
-	todoRepository := repository.NewTodoRepository(db)
 	if err = db.Ping(); err != nil {
 		log.Println(err.Error())
 	}
-
-	todoModels, err := todoRepository.FindAll()
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	log.Println(todoModels)
-
 }
--- service/todo_service.go
+++ service/todo_service.go
@@ -2,23 +2,38 @@ package service
 
 import (
 	"database/sql"
+	"github.com/jinseoko/real-life-go-api/model"
 	"github.com/jinseoko/real-life-go-api/repository"
 )
 
 type TodoService interface {
-}
-
-type todoService struct {
-	todoRepository repository.TodoRepository
+	WithTrx(trx *sql.Tx) TodoService
+	GetTodos(limit, offset int) ([]model.TodoModel, error)
+	CreateTodo(title, content string) (int64, error)
+	GetTodo(int64) (*model.TodoModel, error)
 }
 
 func NewTodoService(tr repository.TodoRepository) TodoService {
 	return todoService{todoRepository: tr}
 }
 
-func (ts todoService) withTrx(trx *sql.Tx) todoService {
+type todoService struct {
+	todoRepository repository.TodoRepository
+}
+
+func (ts todoService) WithTrx(trx *sql.Tx) TodoService {
 	ts.todoRepository = ts.todoRepository.WithTrx(trx)
 	return ts
 }
 
+func (ts todoService) GetTodos(limit int, offset int) ([]model.TodoModel, error) {
+	return ts.todoRepository.FindAll(limit, offset)
+}
 
+func (ts todoService) CreateTodo(title, content string) (int64, error) {
+	return ts.todoRepository.Insert(title, content)
+}
+
+func (ts todoService) GetTodo(id int64) (*model.TodoModel, error) {
+	return ts.todoRepository.FindOne(id)
+}
